4d5a17db22697d2f93f30f22b7afe7ecc8918058
Rezkyns <113870181+Rezkyns@users.noreply.github.com>
2022-09-29 08:52:59 +0800

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,45 @@
+//menu
+var tombolmenu = $(".tombol-menu");
+var menu = $("nav .menu ul");
+
+function klikmenu(){
+    tombolmenu.click(function(){
+        menu.toggle(); 
+    });
+    menu.click(function(){
+        menu.toggle();
+    })
+}
+
+$(document).ready(function() {
+    var width = $(window).width();
+    if(width < 990) {
+        klikmenu();
+    }
+})
+
+
+//cek lebar
+
+$(window).resize(function(){
+    var width = $(window).width();
+    if(width > 989){
+        menu.css("display","block");
+    }else{
+        menu.css("display","none")
+    }
+    klikmenu();
+})
+
+// efek skrol
+$(document).ready(function(){
+    var scroll_pos = 0;
+    $(document).scroll(function (){
+        scroll_pos = $(this).scrollTop();
+        if(scroll_pos > 0){
+            $("nav").addClass("putih");
+        }else{
+            $("nav").removeClass("putih");
+        }
+    })
+})
